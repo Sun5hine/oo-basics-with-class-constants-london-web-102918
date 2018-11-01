f947f0fe0819df3655b81041ed567ac7bef59384
sun5hine <af5kh@yahoo.com>
2018-11-01 18:26:27 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,7 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count, 
+  attr_reader :title, :genre 
 
-  # GENRES = [ ] 
   GENRES = []
   
   def initialize(title)
